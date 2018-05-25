13ab688e877664c63dbc8da54450ee0a92ed4aa6
Paulo Cavalcante <pauloediel.cavalcante@gmail.com>
2018-05-25 19:24:27 -0300

    Botões de mudança do Carrosel

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-(function(win, doc) {
-  'use strit';
-  win.onload = function() {
-    var slides = doc.querySelectorAll('.slide');
-    for (var i = 0; i < slides.length; i++) {
-      slides[i].classList.toggle("slideativo");
-    }
-  };
-})(window, document);
+var slides = document.querySelectorAll('.slide');
+var i = 0;
+var time = 1000;
+var $buttonMudar = document.querySelector('[data-js="muda"]');
+
+
+$buttonMudar.addEventListener('click', clickMudar, false);
+
+function clickMudar() {
+  if (i < slides.length - 1) {
+
+    slides[i].classList.remove("slide-ativo");
+    i++;
+    slides[i].classList.add("slide-ativo");
+  } else {
+    i = 0;
+  }
+}
+
+
+
+
+
+//window.onload = changeSlide();
