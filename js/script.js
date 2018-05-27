9a6dcf5a5b0b1f465c2b266342d7113c32b81a05
Paulo Cavalcante <pauloediel.cavalcante@gmail.com>
2018-05-27 14:22:51 -0300

    Carrossel de notícias finalziado

--- js/script.js
+++ js/script.js
@@ -1,24 +1,20 @@
 var slides = document.querySelectorAll('.slide');
 var i = 0;
-var time = 1000;
-var $buttonMudar = document.querySelector('[data-js="muda"]');
+var time = 3000;
 
+setInterval(mudarSlide, time);
 
-$buttonMudar.addEventListener('click', clickMudar, false);
-
-function clickMudar() {
+function mudarSlide() {
   if (i < slides.length - 1) {
-
     slides[i].classList.remove("slide-ativo");
     i++;
     slides[i].classList.add("slide-ativo");
+
   } else {
+
     i = 0;
   }
+
 }
 
-
-
-
-
-//window.onload = changeSlide();
+window.onload = mudarSlide();
